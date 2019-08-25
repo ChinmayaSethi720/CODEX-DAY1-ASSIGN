fa3de2fdd7507462bb923d1962245c4cc98a5e2b
ChinmayaSethi720 <53614704+ChinmayaSethi720@users.noreply.github.com>
2019-08-25 22:40:44 +0530

    Update question6b.java

--- question6b.java
+++ question6b.java
@@ -1,33 +1,18 @@
-
 public class question6b 
 {
 	public static void main(String[] args) 
 	{
-		int a=2019;	
-		if (a%4==0) 
-			{
-			if (a%100==0) 
-			{
-		        if (a%400==0)
-			{
-		               System.out.println(a+ " is a leap year");
-		        }
-	           	
-				else 
-	                {
-		               	System.out.println(a+ " is not a leap year");
-	    		}
-			
-			}
-				else
-			{
-				System.out.println(a+ " is not a leap year");
-			}
-			
+		int a=2019;
+		
+		if (a % 4 ==0) {
+			if (a % 100 !=0)
+		        	System.out.println(a+ " is a leap year");
+					
+		else if (a % 400 ==0)
+			System.out.println(a+ " is a leap year");
 		}
-				else
-			{
+			else
 				System.out.println(a+ " is not a leap year");
-			}
 	}
 }
+
